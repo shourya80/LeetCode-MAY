cdd5eec3e68b752268fe7f74f430bcda8f364389
Sanjay Prabhu <sanjayprabhu1999@gmail.com>
2020-05-04 16:49:06 +0530

    uploaded questions for all d1-d4.cpp

--- week1-[ 01 - 07 ]/D4.cpp
+++ week1-[ 01 - 07 ]/D4.cpp
@@ -1,3 +1,16 @@
+/*
+Given a positive integer, output its complement number. 
+The complement strategy is to flip the bits of its binary representation.
+*/
+
+/*
+Input: 5
+Output: 2
+
+Input: 1
+Output: 0
+*/
+
 class Solution {
 public:
     int findComplement(int num) 
